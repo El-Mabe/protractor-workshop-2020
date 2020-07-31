ebd84e005e8bf6326bdcf42240393a5bdd432e53
Julian David Mabesoy Jimenez <julian.mabesoy@correo.icesi.edu.co>
2020-07-30 19:28:25 -0500

    disable promise and selenium

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -2,10 +2,12 @@ import { Config, browser } from 'protractor';
 
 import { reporter } from './helpers/reporter';
 
+SELENIUM_PROMISE_MANAGER: false;
+
 export const config: Config = {
   framework: 'jasmine',
   specs: [ '../test/google.spec.js' ],
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
