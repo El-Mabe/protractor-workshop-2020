902666614ab0bbf9bddb7d78d8d3f9b0a0f30db2
Julian David Mabesoy Jimenez <julian.mabesoy@correo.icesi.edu.co>
2020-07-30 22:46:42 -0500

    the coments was fixed

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -2,11 +2,10 @@ import { Config, browser } from 'protractor';
 
 import { reporter } from './helpers/reporter';
 
-SELENIUM_PROMISE_MANAGER: false;
-
 export const config: Config = {
   framework: 'jasmine',
   specs: [ '../test/google.spec.js' ],
+  SELENIUM_PROMISE_MANAGER: false,
   //seleniumAddress: 'http://localhost:4444/wd/hub',
   onPrepare: () => {
     browser.ignoreSynchronization = true;
