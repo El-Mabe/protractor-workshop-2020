06a369fe8aa8cd105aa69820f734ec547f1d6d13
Julian David Mabesoy Jimenez <42072369+El-Mabe@users.noreply.github.com>
2020-07-31 08:32:47 -0500

    Promise selenium (#4)
    * setup protractor configuration
    
    * improve-test
    
    * resolving some conflicts from improve-test
    
    * add report to console
    
    * the conflict in local.config.ts was fixed
    
    * disable promise and selenium
    
    * the coments was fixed

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -5,10 +5,10 @@ import { reporter } from './helpers/reporter';
 export const config: Config = {
   framework: 'jasmine',
   specs: [ '../test/google.spec.js' ],
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  SELENIUM_PROMISE_MANAGER: false,
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
   onPrepare: () => {
     browser.ignoreSynchronization = true;
-
     reporter();
 
   }
